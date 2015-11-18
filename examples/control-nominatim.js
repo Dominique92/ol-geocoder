49004860f1d309e6de4347deb590bea34cacf7d5
jonataswalker <jonataswalker@gmail.com>
2015-11-18 16:19:19 -0200

    Release v1.4.1

--- examples/control-nominatim.js
+++ examples/control-nominatim.js
@@ -1,67 +1,62 @@
 (function(win, doc){
-    'use strict';
-    
-    var 
-        olview = new ol.View({
-            center: [0, 0],
-            zoom: 3,
-            minZoom: 2,
-            maxZoom: 20
-        }),
-        baseLayer = new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'osm'})
-        }),
-        map = new ol.Map({
-            target: doc.getElementById('map'),
-            view: olview,
-            loadTilesWhileAnimating: true,
-            loadTilesWhileInteracting: true,
-            layers: [baseLayer]
-        })
-    ;
+  'use strict';
+  
+  var 
+    olview = new ol.View({
+      center: [0, 0],
+      zoom: 3,
+      minZoom: 2,
+      maxZoom: 20
+    }),
+    baseLayer = new ol.layer.Tile({
+      source: new ol.source.MapQuest({layer: 'osm'})
+    }),
+    map = new ol.Map({
+      target: doc.getElementById('map'),
+      view: olview,
+      loadTilesWhileAnimating: true,
+      loadTilesWhileInteracting: true,
+      layers: [baseLayer]
+    })
+  ;
 
-    //Instantiate with some options and add the Control
-    var geocoder = new Geocoder('nominatim', {
-        provider: 'photon',
-        lang: 'en',
-        placeholder: 'Search for ...',
-        limit: 5,
-        keepOpen: true
-    });
-    map.addControl(geocoder);
-    
-    //Listen when an address is chosen
-    geocoder.on('change:geocoder', function(evt){
-        var
-            feature_id = evt.target.get('geocoder'),
-            feature = geocoder.getSource().getFeatureById(feature_id),
-            address_html = feature.get('address_html'),
-            address_obj = feature.get('address_obj'),
-            address_original = feature.get('address_original'),
-            coord = feature.getGeometry().getCoordinates()
-        ;
-        content.innerHTML = '<p>'+address_html+'</p>';
-        overlay.setPosition(coord);
-        
-        console.info(address_obj);
-    });
-
-    /**
-    * Popup
-    **/
+  //Instantiate with some options and add the Control
+  var geocoder = new Geocoder('nominatim', {
+    provider: 'photon',
+    lang: 'en',
+    placeholder: 'Search for ...',
+    limit: 5,
+    keepOpen: true
+  });
+  map.addControl(geocoder);
+  
+  //Listen when an address is chosen
+  geocoder.on('addresschosen', function(evt){
     var
-        container = doc.getElementById('popup'),
-        content = doc.getElementById('popup-content'),
-        closer = doc.getElementById('popup-closer'),
-        overlay = new ol.Overlay({
-            element: container,
-            offset: [0, -40]
-        })
+      feature = evt.feature,
+      coord = evt.coordinate,
+      address_html = feature.get('address_html')
     ;
-    closer.onclick = function() {
-        overlay.setPosition(undefined);
-        closer.blur();
-        return false;
-    };
-    map.addOverlay(overlay);
+    content.innerHTML = '<p>'+address_html+'</p>';
+    overlay.setPosition(coord);
+  });
+
+  /**
+  * Popup
+  **/
+  var
+    container = doc.getElementById('popup'),
+    content = doc.getElementById('popup-content'),
+    closer = doc.getElementById('popup-closer'),
+    overlay = new ol.Overlay({
+      element: container,
+      offset: [0, -40]
+    })
+  ;
+  closer.onclick = function() {
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
+  };
+  map.addOverlay(overlay);
 })(window, document);
