907190914e1f96e9149e78b2375c3afe96fec489
Jonatas Walker <jonataswalker@gmail.com>
2020-09-26 19:47:30 -0300

    Fix tests

--- test/unit/instance.test.js
+++ test/unit/instance.test.js
@@ -3,7 +3,7 @@ import LayerVector from 'ol/layer/Vector';
 import SourceVector from 'ol/source/Vector';
 
 // eslint-disable-next-line no-shadow
-import Geocoder from '../..';
+import Geocoder from '../../dist/ol-geocoder';
 import { DEFAULT_OPTIONS, PROVIDERS } from '../../konstants';
 
 const options = {
